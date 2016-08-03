b774a2afa065a51664832c0d7cf3f5ef1a8e3759
David Mrnustik <david.mrnustik@hotmail.com>
2016-08-03 21:17:53 +0200

    Change ignore policy in src only to vendor, cvs parse in controller, test forms and table view

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('FoundersMapQuest', ['leaflet-directive','ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/', {
+		controller: 'MainController',
+		templateUrl: 'views/main.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
---
+++ src/js/controllers/MainController.js
@@ -0,0 +1,50 @@
+app.controller('MainController', ['$scope', function($scope){
+
+  angular.extend($scope, {
+    mapCenter: {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 4
+    },
+    defaults: {
+      tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+      zoomControlPosition: 'topright',
+      tileLayerOptions: {
+        opacity: 0.9,
+        detectRetina: true,
+        reuseTiles: true,
+      },
+      scrollWheelZoom: false
+    }
+  });
+
+  $scope.csvData = "";
+  $scope.delimiter = ",";
+  $scope.startups = [];
+
+  $scope.$watchGroup(["delimiter", "csvData"], function() {
+  
+    var lines, data, length, place;
+    $scope.startups = [];
+    $scope.places = [];
+    lines = $scope.csvData.split('\n');
+    
+    for (var i = lines.length - 1; i >= 0; i--) {
+      places = [];
+      line = lines[i];
+      data = line.split($scope.delimiter);
+
+      var name = data[0];
+      var lat = data[1];
+      var lng = data[2];
+
+      $scope.startups.push({
+        name: name,
+        lat: parseFloat(lat),
+        lng: parseFloat(lng)
+      });
+    }
+    
+  });
+
+}]);
